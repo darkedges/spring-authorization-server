971d7d54f8b097e14a4bb1a1971bc3eaa530dbc9
DarkEdges <nirving.darkedges@darkedges.com>
2022-12-25 09:18:15 +1100

    Cleaned up private key jwt example
    This brings it into alignment with other examples

--- samples/private-key-jwt-authorizationserver/src/main/java/com/darkedges/privatekeyjwt/config/AuthorizationServerConfig.java
+++ samples/private-key-jwt-authorizationserver/src/main/java/com/darkedges/privatekeyjwt/config/AuthorizationServerConfig.java
@@ -22,6 +22,7 @@ import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.util.UUID;
 
+import com.darkedges.privatekeyjwt.jose.Jwks;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
@@ -32,6 +33,7 @@ import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.oidc.OidcScopes;
 import org.springframework.security.oauth2.jose.jws.SignatureAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
@@ -49,19 +51,6 @@ import com.nimbusds.jose.proc.SecurityContext;
 
 @Configuration(proxyBeanMethods = false)
 public class AuthorizationServerConfig {
-	private static RSAKey generateRsa() throws NoSuchAlgorithmException {
-		KeyPair keyPair = generateRsaKey();
-		RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
-		RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
-		return new RSAKey.Builder(publicKey).privateKey(privateKey).keyID(UUID.randomUUID().toString()).build();
-	}
-
-	private static KeyPair generateRsaKey() throws NoSuchAlgorithmException {
-		KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-		keyPairGenerator.initialize(2048);
-		return keyPairGenerator.generateKeyPair();
-	}
-
 	@Bean
 	public RegisteredClientRepository registeredClientRepository() {
 		String jwkSetUri = "http://127.0.0.1:9000/resources";
@@ -87,14 +76,14 @@ public class AuthorizationServerConfig {
 	}
 
 	@Bean
-	public JWKSource<SecurityContext> jwkSource() throws NoSuchAlgorithmException {
-		RSAKey rsaKey = generateRsa();
+	public JWKSource<SecurityContext> jwkSource() {
+		RSAKey rsaKey = Jwks.generateRsa();
 		JWKSet jwkSet = new JWKSet(rsaKey);
 		return (jwkSelector, securityContext) -> jwkSelector.select(jwkSet);
 	}
 
 	@Bean
 	public AuthorizationServerSettings authorizationServerSettings() {
-		return AuthorizationServerSettings.builder().issuer("http://localhost:9000").build();
+		return AuthorizationServerSettings.builder().build();
 	}
 }
